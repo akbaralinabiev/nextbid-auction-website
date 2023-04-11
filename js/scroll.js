f07f63463eb78689518a5b3c3873b23df5beb391
akbaralinabiev <akbaralinabiev14@gmail.com>
2023-04-12 01:08:33 +0200

    new items page responsive changes, contact section button design

--- js/scroll.js
+++ js/scroll.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
     $(window).scroll(function() {
       var scrollTop = $(this).scrollTop();
-      var pixelsToMove = scrollTop / 12;
-      $("#hero").css("background-position", "top " + (35 - pixelsToMove) + "% right 0");
+      var pixelsToMove = scrollTop / 15;
+      $("#hero, .intro").css("background-position", "top " + (35 - pixelsToMove) + "% right 0");
     });
   });
   
