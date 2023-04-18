8f09e20e7dc91d3a663f4dd549b85341f80fd471
akbaralinabiev <akbaralinabiev14@gmail.com>
2023-04-18 12:32:01 +0200

    checking

--- js/scroll.js
+++ js/scroll.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $(window).scroll(function() {
       var scrollTop = $(this).scrollTop();
       var pixelsToMove = scrollTop / 15;
-      $("#hero, .intro").css("background-position", "top " + (35 - pixelsToMove) + "% right 0");
+      $(".intro").css("background-position", "top " + (35 - pixelsToMove) + "% right 0");
     });
   });
   
