7c2c68708987ed9817bcb1e6ffce5343ec7c476e
akbaralinabiev <akbaralinabiev14@gmail.com>
2023-05-01 06:11:49 +0200

    leaving only in one page

--- js/like-counter.js
+++ js/like-counter.js
@@ -1,45 +1,28 @@
 const likeButtons = document.querySelectorAll('.like-button');
+const numberOfLikesElements = document.querySelectorAll('.like-count');
 
-likeButtons.forEach((button) => {
-  const likeCount = button.nextElementSibling;
-  let count = 0;
-  
-  button.addEventListener('click', () => {
-    count++;
-    likeCount.innerText = count;
-  });
-});
+for (let i = 0; i < likeButtons.length; i++) {
+  const likeBtn = likeButtons[i];
+  const numberOfLikesElement = numberOfLikesElements[i];
 
+  let numberOfLikes = Number.parseInt(numberOfLikesElement.textContent, 10);
+  let isLiked = false;
 
+  const likeClick = () => {
+    if (!isLiked) {
+      likeBtn.classList.add('isLiked');
+      numberOfLikes++;
+      numberOfLikesElement.textContent = numberOfLikes;
+      isLiked = !isLiked;
+    } else {
+      likeBtn.classList.remove('isLiked');
+      numberOfLikes--;
+      numberOfLikesElement.textContent = numberOfLikes;
+      isLiked = !isLiked;
+    }
+  };
 
-// const likeButton = document.querySelector('.like-button');
-//   const likeIcons = likeButton.querySelectorAll('img');
-//   const likeCount = document.querySelector('.like-count');
-
-//   likeButton.addEventListener('click', function() {
-//     if (likeButton.classList.contains('liked')) {
-//       // User unlikes the product
-//       likeCount.innerText = parseInt(likeCount.innerText) - 1;
-//       likeIcons[0].classList.remove('hidden');
-//       likeIcons[1].classList.add('hidden');
-//       likeButton.classList.remove('liked');
-//       localStorage.removeItem('likedProduct');
-//     } else {
-//       // User likes the product
-//       likeCount.innerText = parseInt(likeCount.innerText) + 1;
-//       likeIcons[0].classList.add('hidden');
-//       likeIcons[1].classList.remove('hidden');
-//       likeButton.classList.add('liked');
-//       localStorage.setItem('likedProduct', true);
-//     }
-//   });
-
-//   // Check if the user has already liked the product
-//   if (localStorage.getItem('likedProduct') === 'true') {
-//     likeIcons[0].classList.add('hidden');
-//     likeIcons[1].classList.remove('hidden');
-//     likeButton.classList.add('liked');
-//     likeCount.innerText = '1';
-//   }
+  likeBtn.addEventListener('click', likeClick);
+}
 
 
--- js/search-filter.js
+++ js/search-filter.js
@@ -1,40 +1,44 @@
-console.log("Search input changed");
-
-
 function searchProducts() {
-    const searchInput = document.querySelector(".search__input");
-    const auctionCards = document.querySelectorAll(".auction-card");
-    const searchResults = document.querySelector(".search__results");
-    const searchTerm = searchInput.value.toLowerCase();
-    let matchFound = false;
-  
-    auctionCards.forEach(card => {
-      const cardTitleElement = card.querySelector(".card-title");
-      const cardTitle = cardTitleElement.textContent.toLowerCase();
-  
-      if (cardTitle.includes(searchTerm)) {
-        card.style.display = "block";
-        matchFound = true;
-      } else {
-        card.style.display = "none";
-      }
-    });
-  
-    if (!matchFound) {
-      if (searchTerm !== "") {
-        const message = document.createElement("p");
-        message.textContent = "No matching products were found.";
-        searchResults.appendChild(message);
-      }
-    } else {
-      // Remove the "no results" message if it exists
-      const message = searchResults.querySelector("p");
-      if (message) {
-        searchResults.removeChild(message);
-      }
-    }
-  }
-  
   const searchInput = document.querySelector(".search__input");
-  searchInput.addEventListener("input", searchProducts);
-  
+  const auctionCards = document.querySelectorAll(".auction-card");
+  const searchResults = document.querySelector(".search__results");
+  const searchTerm = searchInput.value.toLowerCase();
+  let matchFound = false;
+
+  auctionCards.forEach(card => {
+    const cardTitleElement = card.querySelector(".card-title");
+    const cardTitle = cardTitleElement.textContent.toLowerCase();
+
+    if (cardTitle.includes(searchTerm)) {
+      card.style.display = "block";
+      matchFound = true;
+    } else {
+      card.style.display = "none";
+    }
+  });
+
+  // Remove the "no results" message if it exists
+  const message = searchResults.querySelector(".no-results-message");
+  if (message) {
+    searchResults.removeChild(message);
+  }
+
+  // Check if any matching products were found
+  if (!matchFound && searchTerm !== "") {
+    const message = document.createElement("p");
+    message.textContent = "No matching products were found.";
+    message.classList.add("no-results-message");
+    searchResults.appendChild(message);
+    const paginationSection = document.querySelector(".pagination-section");
+    paginationSection.style.display = "none";
+  } else {
+    const paginationSection = document.querySelector(".pagination-section");
+    paginationSection.style.display = "block";
+  }
+}
+
+const searchInput = document.querySelector(".search__input");
+searchInput.addEventListener("input", searchProducts);
+
+
+
