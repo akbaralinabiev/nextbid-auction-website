476b01e5f0d1de71885c33cf3bc96785eb805146
akbaralinabiev <akbaralinabiev14@gmail.com>
2023-05-15 10:10:07 +0200

    loader added

--- js/choose-image.js
+++ js/choose-image.js
@@ -1,22 +1,3 @@
-// const fileInput = document.getElementById("image-upload");
-// const fileInfo = document.querySelector(".file-info");
-
-// fileInput.addEventListener("change", function() {
-//   if (fileInput.value) {
-//     fileInfo.textContent = fileInput.files[0].name;
-//   } else {
-//     fileInfo.textContent = "No file chosen";
-//   }
-// });
-
-// function cancelFile() {
-//   var fileUpload = document.getElementById("image-upload");
-//   var fileInfo = document.querySelector(".file-info");
-//   fileUpload.value = ""; // Clear the selected file
-//   fileInfo.textContent = ""; // Clear the file info text
-// }
-
-
 const fileInput = document.getElementById("image-upload");
 const fileInfo = document.querySelector(".file-info");
 
---
+++ js/lodaer.js
@@ -0,0 +1,7 @@
+setTimeout(function() {
+  var redirected = sessionStorage.getItem("redirected");
+  if (!redirected) {
+    sessionStorage.setItem("redirected", "true");
+    window.location.href = "main.html";
+  }
+}, 3000);
